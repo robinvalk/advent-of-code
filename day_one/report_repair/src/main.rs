405b257ca8d18f46ba3b6ba82fe0764509ef5706
Robin Valk <robin.valk@merapar.com>
2020-12-02 18:33:55 +0100

    Refactored day one

--- day_one/report_repair/src/main.rs
+++ day_one/report_repair/src/main.rs
@@ -1,57 +1,52 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+struct Input {
+    filename: String,
+}
 
-    let input_file: String = get_file_from_args(&args);
-    let input_string = load_contents_from_file(input_file);
-    let input_vec = convert_input_to_integer_vector(input_string);
-    
-    'answer_one: for a in input_vec.iter().copied() {
-        for b in input_vec.iter().copied() {
+impl Input {
+    fn new(filename: String) -> Input {
+        let instance = Input { filename };
+        instance.load_data();
+
+        instance
+    }
+
+    fn load_data(&self) -> Vec<u32> {
+        return self.file_contents().lines()
+            .map(|line| line.parse::<u32>().unwrap())
+            .collect();
+    }
+
+    fn file_contents(&self) -> String {
+        println!("Loading contents from file: {}", self.filename);
+
+        return fs::read_to_string(&self.filename).expect("Something went wrong loading contents from file");
+    }
+}
+
+fn main() {
+    let filename = env::args().nth(1).unwrap_or("input.txt".to_string());
+    let input_data = Input::new(filename).load_data();
+
+    'answer_one: for a in input_data.iter().copied() {
+        for b in input_data.iter().copied() {
             if (a + b) == 2020 {
-                println!("Answer is: {}", a*b);
+                println!("Answer one: {}", a*b);
                 break 'answer_one;
             }
         }
     }
-    
-    'answer_two: for a in input_vec.iter().copied() {
-        for b in input_vec.iter().copied() {
-            for c in input_vec.iter().copied() {
+
+    'answer_two: for a in input_data.iter().copied() {
+        for b in input_data.iter().copied() {
+            for c in input_data.iter().copied() {
                 if (a + b + c) == 2020 {
-                    println!("Answer is: {}", a*b*c);
+                    println!("Answer two: {}", a*b*c);
                     break 'answer_two;
                 }
             }
         }
     }
-}
-
-fn get_file_from_args(args: &[String]) -> String {
-    if args.len() > 1 {
-        return args[1].clone();
-    }
-
-    return "input.txt".to_string();
-}
-
-fn load_contents_from_file(filename: String) -> String {
-    println!("Loading input from file: {}", filename);
-    let input_string = fs::read_to_string(filename)
-        .expect("Something went wrong reading the input file");
-
-    return input_string;
-}
-
-fn convert_input_to_integer_vector(input_string: String) -> Vec<u32> {
-    let mut input: Vec<u32> = Vec::new();
-
-    let input_iterator = input_string.split("\n");
-    for elem in input_iterator {
-        input.push(elem.parse::<u32>().unwrap());
-    }
-
-    return input;
 }

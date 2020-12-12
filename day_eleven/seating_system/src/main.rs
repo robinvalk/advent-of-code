62837321f7d2e8eedcd3a48b99c727cd6ccbdd7b
Robin Valk <robin.valk@merapar.com>
2020-12-12 16:52:55 +0100

    Day eleven, failure, need to redo. Continuing to day twelve

--- day_eleven/seating_system/src/main.rs
+++ day_eleven/seating_system/src/main.rs
@@ -10,65 +10,62 @@ struct OccupiedSeatCounter {
 impl OccupiedSeatCounter {
     fn count(&mut self) -> i32 {
         let mut rounds = 0;
-        let mut seats_changed = 1;
-        while seats_changed > 0 {
-            seats_changed = self.process();
+
+        loop {
+            let new_layout = self.process();
             rounds += 1;
-            println!("Seats changed: {}", seats_changed);
+            // println!("Round: {}", rounds);
+
+            if self.layouts_equal(&new_layout) {
+                break;
+            } else {
+                self.layout = new_layout;
+            }
         }
 
         println!("Rounds: {}", rounds);
 
-        self.layout.iter().filter(|(_key, seat)| **seat == '#').count() as i32
+        self.layout.iter().filter(|(_key, seat)| *seat.clone() == '#').count() as i32
     }
 
-    fn process(&mut self) -> i32 {
-        let layout_copy: HashMap<String, char> = self.get_layout_copy();
-        let mut seats_changed = 0;
+    fn process(&self) -> HashMap<String, char> {
+        let mut editable_layout: HashMap<String, char> = HashMap::new();
 
         for row_index in 0 .. self.size.0 { 0;
-            for seat_index in 0 .. self.size.1 {
-                let seat_key = self.get_seat_key(row_index as i32, seat_index as i32);
-                let seat = layout_copy.get(&seat_key).unwrap();
+            for column_index in 0 .. self.size.1 {
+                let seat_key = self.get_seat_key(row_index as i32, column_index as i32);
+                let seat = self.layout.get(&seat_key).unwrap().clone();
 
-                let occupied = self.count_adjacent_seat_occupied(&layout_copy, row_index, seat_index);
-                if *seat == 'L' && occupied == 0 {
-                    self.layout.insert(seat_key, '#');
-                } else if *seat == '#' && occupied >= 4 {
-                    self.layout.insert(seat_key, 'L');
+                let occupied = self.count_adjacent_seat_occupied(row_index, column_index);
+                if seat == 'L' && occupied == 0 {
+                    editable_layout.insert(seat_key, '#');
+                } else if seat == '#' && occupied >= 4 {
+                    editable_layout.insert(seat_key, 'L');
                 } else {
-                    continue;
+                    editable_layout.insert(seat_key, seat);
                 }
-
-                seats_changed += 1;
             }
         }
 
-        seats_changed
+        editable_layout
     }
 
-    fn count_adjacent_seat_occupied(&self, layout: &HashMap<String, char>, row_index: usize, seat_index: usize) -> i32 {
+    fn count_adjacent_seat_occupied(&self, row_index: usize, column_index: usize) -> i32 {
         let mut count = 0;
 
         for iter_row_index in [-1, 0, 1].iter() {
-            for iter_seat_index in [-1, 0, 1].iter() {
-                if *iter_row_index == 0 && *iter_seat_index == 0 {
+            for iter_column_index in [-1, 0, 1].iter() {
+                if *iter_row_index == 0 && *iter_column_index == 0 {
                     continue;
                 }
 
                 let ri = row_index as i32 + iter_row_index;
-                let ci = seat_index as i32 + iter_seat_index;
-
-                if ri < 0 || ri >= self.size.0 as i32 {
-                    continue;
-                }
-
-                if ci < 0 || ci >= self.size.1 as i32 {
-                    continue;
-                }
+                let ci = column_index as i32 + iter_column_index;
 
                 let seat_key = self.get_seat_key(ri, ci);
-                if *layout.get(&seat_key).unwrap() == '#' {
+                let seat_status = self.layout.get(&seat_key).unwrap_or(&'.').clone();
+
+                if seat_status == '#' {
                     count += 1;
                 }
             }
@@ -81,7 +78,7 @@ impl OccupiedSeatCounter {
         let mut layout_copy: HashMap<String, char> = HashMap::new();
 
         for (key, value) in self.layout.iter() {
-            layout_copy.insert(key.as_str().to_string(), *value);
+            layout_copy.insert(String::from(key), value.clone());
         }
 
         layout_copy
@@ -89,6 +86,19 @@ impl OccupiedSeatCounter {
 
     fn get_seat_key(&self, row_index: i32, seat_index: i32) -> String {
         format!("{}{}", row_index, seat_index)
+    }
+
+    fn layouts_equal(&self, new_layout: &HashMap<String, char>) -> bool {
+        self.layout.iter().filter(|(key, value)| {
+            let key_ref = &key.to_string();
+            let val = new_layout.get(key_ref);
+
+            if val.is_none() {
+                println!("{}", key);
+            }
+
+            !new_layout.contains_key(key_ref) || val.is_none() || val.unwrap() != value.clone()
+        }).count() == 0
     }
 }
 
@@ -130,6 +140,7 @@ fn main() {
     let input = Input::new(input_filename);
 
     let mut occupied_seat_counter = input.get_occupied_seat_counter();
+    println!("Rows: {}, Columns: {}", occupied_seat_counter.size.0, occupied_seat_counter.size.1);
 
     println!("Answer one: {}", occupied_seat_counter.count())
 }
